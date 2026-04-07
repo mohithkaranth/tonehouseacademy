2c2c06e94129ac4ef261e25cf103ff0cf7df340f
mohithkaranth <mkaranth@gmail.com>
2026-04-07 11:59:16 +0800

    final working build

--- prisma.config.ts
+++
@@ -1,9 +0,0 @@
-import "dotenv/config";
-import { defineConfig } from "prisma/config";
-
-export default defineConfig({
-  schema: "prisma/schema.prisma",
-  db: {
-    url: process.env.DATABASE_URL,
-  },
-});
